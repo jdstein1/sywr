623fa5476e5ad8eb87e367afd3e6bfe9c2d614d7
jdstein1 <jdstein1@gmail.com>
2015-01-11 20:32:10 -0600

    Sorting for member table

--- src/js/games.js
+++ src/js/games.js
@@ -7,30 +7,38 @@
 		var stitle = 'sDataGames';
 		console.log('START', stitle);
 
-		var DataDestiny = ['test1', 'test2', 'test3']
+		var myGameData = [{
+			"type":["rpg","mmo","fps"],
+			"name":"Destiny",
+			"publisher":"Activision",
+			"developer":"Bungie"
+		}];
+		console.log('myGameData: ', myGameData);
 
-		return DataDestiny;
+		return DataGames;
 	}
 	// console.log('test sDataGames:', angular.module('app').service('sDataGames'));
 
-	app.service('sDataDestiny', sDataDestiny)
-	function sDataDestiny() {
-		var stitle = 'sDataDestiny';
+	app.service('sDataGameDestiny', sDataGameDestiny)
+	function sDataGameDestiny() {
+		var stitle = 'sDataGameDestiny';
 		console.log('START', stitle);
 
 		var DataDestiny = ['test1', 'test2', 'test3']
 
 		return DataDestiny;
 	}
-	// console.log('test sDataDestiny:', angular.module('app').service('sDataDestiny'));
+	// console.log('test sDataGameDestiny:', angular.module('app').service('sDataGameDestiny'));
 
 	app.controller('cGames', cGames)
-	function cGames($scope, sDataDestiny) {
+	function cGames($scope, sDataGameDestiny) {
 		$scope.ctitle = 'cGames';
 		$scope.title = 'Games';
 		console.log('START', $scope.ctitle);
 
-		$scope.DataDestiny = sDataDestiny;
+		$scope.sortClass = "def";
+
+		$scope.DataDestiny = sDataGameDestiny;
 		console.log('$scope.DataDestiny', $scope.DataDestiny);
 	}
 	// console.log('test cGames:', angular.module('app').controller('cGames'));
--- src/js/members.js
+++ src/js/members.js
@@ -8,22 +8,22 @@
 		console.log('START', stitle);
 
 		var myMemberData = [{
-		"id":"8310647",
-		"handle":"SarcasticWatchr",
-		"name":"Jeff"
-			},{
-		"id":"7037048",
-		"handle":"Spacewolf 024",
-		"name":"John"
-			},{
-		"id":"7666141",
-		"handle":"kras07",
-		"name":"Kras"
-			},{
-		"id":"3892477",
-		"handle":"Tanman2391",
-		"name":"Tanner"
-			}];
+			"id":"8310647",
+			"handle":"SarcasticWatchr",
+			"name":"Jeff"
+		},{
+			"id":"7037048",
+			"handle":"Spacewolf 024",
+			"name":"John"
+		},{
+			"id":"7666141",
+			"handle":"kras07",
+			"name":"Kras"
+		},{
+			"id":"3892477",
+			"handle":"Tanman2391",
+			"name":"Tanner"
+		}];
 		console.log('myMemberData: ', myMemberData);
 
 		return myMemberData;
@@ -37,20 +37,49 @@
 		$scope.title = 'Members';
 		console.log('START', $scope.ctitle);
 
-		$scope.myMemberData = sDataMembers;
-		console.log('$scope.myMemberData: ', $scope.myMemberData);
-		console.log('$scope.myMemberData[1]: ', $scope.myMemberData[1]);
+		$scope.sortClass = {};
+		$scope.sortClass.name = "def";
+		$scope.sortClass.handle = "def";
+		$scope.sortClass.id = "def";
 
-		$scope.myMemberOrder = [];
+		$scope.myMemberData = sDataMembers;
+		// console.log('$scope.myMemberData: ', $scope.myMemberData);
+		// console.log('$scope.myMemberData[1]: ', $scope.myMemberData[1]);
+
+		$scope.myMemberOrder = '-name';
+		$scope.reverse = false;
+
+		if ($scope.myMemberOrder) {
+			console.log('$scope.myMemberOrder');
+		} else {
+			console.log('$scope.myMemberOrder');
+		}
+
+		$scope.myMemberOrderToggle = function (type) {
+			console.log('myMemberOrderToggle:', type +'; type:'+ $scope.sortClass[type]);
+
+			// for (var i = 0; i < $scope.sortClass.length; i++) {
+			// 	console.log('$scope.sortClass[i]: ', $scope.sortClass[i]);
+			// 	$scope.sortClass[i] = 'def';
+			// };
+
+			if ($scope.sortClass[type] === 'des') {
+				$scope.sortClass[type] = 'asc';
+			} else {
+				$scope.sortClass[type] = 'des';
+			}
+			$scope.reverse=!$scope.reverse;
+			console.log('$scope.sortClass: ', $scope.sortClass);
+		};
 
 		$scope.myMembersUrls = [];
 
 		for (var i = 0; i < $scope.myMemberData.length; i++) {
 			var myMemberUrl = constProtocol[0] + '://' + constBungieUrl.domain +'/'+ constBungieUrl.lang +'/'+ constBungieUrl.profile + '/254/' + $scope.myMemberData[i].id;
-			console.log('myMemberUrl: ', myMemberUrl);
+			// console.log('myMemberUrl: ', myMemberUrl);
 			$scope.myMemberData[i].url = myMemberUrl;
 		};
-		console.log('$scope.myMemberData: ', $scope.myMemberData);
+		// console.log('$scope.myMemberData: ', $scope.myMemberData);
 	}
 	// console.log('test cMembers:', angular.module('app').controller('cMembers'));
 
